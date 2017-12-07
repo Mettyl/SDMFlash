6f62063c5d1f7328df4dfcd27b38be54ede2b337
Dominik Dvořáček <dominikdvoracek1@gmail.com>
2017-12-07 20:30:16 +0100

    Whole database system
    Added:
    -Room database implemented into project (using Gradle)
    -dataTypes package
      -Converters
      -DataTuple
      -File
      -Language
    -structure package
      -AccessExecutor
      -AppDatabase
      -Word
      -WordDao
    -DbTest file: for testing the structure and showing the workflow.

--- app/src/main/java/com/sdm/sdmflash/HomeActivity.java
+++ app/src/main/java/com/sdm/sdmflash/HomeActivity.java
@@ -1,17 +1,31 @@
 package com.sdm.sdmflash;
 
+import android.arch.persistence.room.Room;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.sdm.sdmflash.db.DbTest;
+import com.sdm.sdmflash.db.structure.AccessExecutor;
+import com.sdm.sdmflash.db.dataTypes.DatesTuple;
+import com.sdm.sdmflash.db.structure.AppDatabase;
+import com.sdm.sdmflash.db.structure.Word;
+import com.sdm.sdmflash.db.dataTypes.File;
+import com.sdm.sdmflash.db.dataTypes.Language;
+
+import java.util.Date;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //spustí test
+        new DbTest().test(getApplicationContext());
     }
 
-    public void doNothing(){
-        boolean nothing = true;
-    }
 }
---
+++ app/src/main/java/com/sdm/sdmflash/db/structure/AccessExecutor.java
@@ -0,0 +1,25 @@
+package com.sdm.sdmflash.db.structure;
+
+import android.content.AbstractThreadedSyncAdapter;
+import android.support.annotation.NonNull;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Třída která obstarává vytvoření nového vlákna. Všechny přístupu k databázi musejí být vedeny
+ * skrze instanci této třídy.
+ * Created by Dominik on 06.12.2017.
+ */
+
+public class AccessExecutor implements Executor {
+
+    /**
+     * Spustí runnable objekt v novém vlákně.
+     * Všechny přístupu k databázi musejí být vedeny skrze tuto metodu!
+     * @param runnable vniřní runnable třída obsahující kód pracující s databází
+     */
+    @Override
+    public void execute(@NonNull Runnable runnable) {
+        new Thread(runnable).start();
+    }
+}
